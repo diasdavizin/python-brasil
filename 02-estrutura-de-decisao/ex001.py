7e8aae5ec97d4b0115f8bf21ab62fede873132f8
Davi de Araújo Dias <davizin042005@gmail.com>
2024-04-20 17:29:43 -0300

    Create ex001.py

---
+++ 02-estrutura-de-decisao/ex001.py
@@ -0,0 +1,10 @@
+'''Faça um Programa que peça dois números e imprima o maior deles.
+'''
+
+n1 = float(input("Digite um número: "))
+n2 = float(input("Digite outro número: "))
+
+if n1 > n2:
+  print("O maior número é o",n1)
+else:
+  print("O maior número é o",n2)
